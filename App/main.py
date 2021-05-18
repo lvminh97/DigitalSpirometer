8f5ddacf27bdf9f694ee98f7164b1fe9efc58629
unknown <lvminh97@gmail.com>
2021-05-18 10:21:30 +0700

    Update code for app

---
+++ App/main.py
@@ -0,0 +1,75 @@
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import QDir, QSize, QSizeF, Qt, QUrl
+from PyQt5.QtGui import QTransform, QIcon
+from PyQt5.QtWidgets import (QApplication, QComboBox, QFileDialog, QFrame,
+        QLineEdit, QListWidget, QPlainTextEdit, QLabel, 
+        QPushButton, QSlider, QStyle, QVBoxLayout,
+        QWidget, QTableWidget, QTableWidgetItem,
+        QMessageBox, QCheckBox, QGroupBox)
+import os
+import sys
+import warnings
+from PlotCanvas import PlotCanvas
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.data = {}
+
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Digital Spirometer - App")
+        self.setGeometry(60, 60, 1200, 650)
+        self.setFixedSize(1000, 650)
+
+        self.label1 = QLabel(self)
+        self.label1.setText("Chọn cổng")
+        self.label1.resize(80, 20)
+        self.label1.move(20, 20)
+
+        self.portList = QComboBox(self)
+        self.portList.resize(80, 25)
+        self.portList.move(100, 20)
+        self.portList.addItem("COM 1")
+        self.portList.addItem("COM 2")
+
+        self.connectBtn = QPushButton("Kết nối", self)
+        self.connectBtn.resize(100, 28)
+        self.connectBtn.move(210, 19)
+        self.connectBtn.clicked.connect(self.connectPort)
+
+        self.graphView = PlotCanvas(self, width = 7, height = 4.5)
+        self.graphView.move(320, 80)
+        self.graphView.plot(list(range(400, 701, 10)), [1] + [0] * 30, 'w')
+
+        self.resultBox = QGroupBox('Tỷ lệ pha màu', self)
+        self.resultBox.resize(270, 250)
+        self.resultBox.move(20, 80)
+        # self.ratioText = QLabel(self)
+        # self.ratioText.resize(200, 180)
+        # self.ratioText.move(350, 30)
+        # self.ratioText.setStyleSheet("font-weight:bold;")
+
+        self.show()
+
+    def connectPort(self):
+        pass
+
+    def handleData(self):
+        pass
+
+    def setColorBox(self, box, RGB): ## box = 0 -> sampleColor; box = 1 -> computeColor
+        styleSheet = 'border: 1px solid #a9a8b3; background-color: #' + '%02x%02x%02x' % (RGB[0], RGB[1], RGB[2])   
+        if box == 0:
+            self.sampleColorBox.setStyleSheet(styleSheet)
+        else:
+            self.computeColorBox.setStyleSheet(styleSheet)     
+
+if __name__ == '__main__':
+    os.system("color 0a")
+    os.system("cls")
+    warnings.filterwarnings('ignore') 
+    app = QApplication(sys.argv)
+    exe = App()
+    sys.exit(app.exec_())
