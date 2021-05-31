a84620b5f9ab646208228bf54e37dbcbd41e09f8
lvminh97 <lvminh97@gmail.com>
2021-06-01 06:29:30 +0700

    Update set no flow control in function connect()

--- JavaApp/src/main/Controller.java
+++ JavaApp/src/main/Controller.java
@@ -71,7 +71,8 @@ public class Controller implements ActionListener, EventListener, SerialPortEven
 		try{
 			commPort = this.selectedPortIdentifier.open("{cmd:detect}\n", TIMEOUT);
 			this.serialPort = (SerialPort) commPort;
-			this.serialPort.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			this.serialPort.setSerialPortParams(4800, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			this.serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
 			this.isConnect = true;
 			this.view.getConnectBtn().setText("Ngắt kết nối");
 //			System.out.println(selectedPort + " opened successfully!!!");
