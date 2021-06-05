61f30cba478c876b297b5b2f000a6d8cd5f586f9
lvminh97 <lvminh97@gmail.com>
2021-06-06 02:12:06 +0700

    Update the diameter of tubes, update function measure
    Update the diameter of tubes, update function measure

--- Firmware/Core/Inc/mpxv7002.h
+++ Firmware/Core/Inc/mpxv7002.h
@@ -4,8 +4,8 @@
 #include "main.h"
 #include "math.h"
 
-#define A1 0.024	// diameter of large tube, unit: m
-#define A2 0.019	// diameter of narrow tube, unit: m
+#define A1 0.0277	// diameter of large tube, unit: m
+#define A2 0.0220	// diameter of narrow tube, unit: m
 #define p	 2			// density of air - here is CO2, unit: kg/m3
 
 float c1, c2;			// coefficients in formulus that compute flow rate
