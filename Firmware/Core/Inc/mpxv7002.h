a468dda0157f96dba43c937c5b67eb67500a86b7
lvminh97 <lvminh97@gmail.com>
2021-06-06 04:48:50 +0700

    Update accuracy of measurement result

--- Firmware/Core/Inc/mpxv7002.h
+++ Firmware/Core/Inc/mpxv7002.h
@@ -5,7 +5,7 @@
 #include "math.h"
 
 #define A1 0.0277	// diameter of large tube, unit: m
-#define A2 0.0220	// diameter of narrow tube, unit: m
+#define A2 0.019	// diameter of narrow tube, unit: m
 #define p	 2			// density of air - here is CO2, unit: kg/m3
 
 float c1, c2;			// coefficients in formulus that compute flow rate
@@ -18,8 +18,8 @@ void mpxv7002_init(void){ // init coefficients
 
 float mpxv7002_get_pressure(uint16_t adc){
 	float volt;
-	volt = (adc - 117) / 4095.0 * 3300; // convert adc value into voltage (range 3300mV)
-	return fabs(volt - 1650) / 1650 * 2000;  // conpute pressure between 2 pipe of sensor
+	volt = (adc - 146) / 4032.0 * 3300; // convert adc value into voltage (range 3300mV)
+	return (volt - 1650) / 1650 * 2000;  // conpute pressure between 2 pipe of sensor
 }
 
 float mpxv7002_get_flowrate(float pressure){
